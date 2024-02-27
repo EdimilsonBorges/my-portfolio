1fe3055d7b6d570a7fd67006cfe3e500d92ce37a
Edimilson Borges <104403198+EdimilsonBorges@users.noreply.github.com>
2024-02-26 21:43:49 -0300

    feat: juntando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { SobreComponent } from './sobre/sobre.component';
-import { HabilidadesComponent } from './habilidades/habilidades.component';
-import { SkillComponent } from './habilidades/skill/skill.component';
-import { ExperienciasComponent } from './experiencias/experiencias.component';
-import { ItemComponent } from './experiencias/item/item.component';
-import { PortifolioComponent } from './portifolio/portifolio.component';
-import { ItemPortifolioComponent } from './portifolio/item-portifolio/item-portifolio.component';
-import { ContactComponent } from './contact/contact.component';
-import { ScrollAnimationDirective } from './app.scroll-animation.directive';
-import { InicioComponent } from './inicio/inicio.component';
+import { PrincipalModule } from './principal/principal.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent,
-    SobreComponent,
-    HabilidadesComponent,
-    SkillComponent,
-    ExperienciasComponent,
-    ItemComponent,
-    PortifolioComponent,
-    ItemPortifolioComponent,
-    ContactComponent,
-    ScrollAnimationDirective,
-    InicioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    PrincipalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/principal/principal.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ContactComponent } from './contact/contact.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { PrincipalComponent } from './principal.component';
+import { ItemComponent } from './experiencias/item/item.component';
+import { SkillComponent } from './habilidades/skill/skill.component';
+import { ItemPortifolioComponent } from './portifolio/item-portifolio/item-portifolio.component';
+import { ScrollAnimationDirective } from './app.scroll-animation.directive';
+
+
+
+@NgModule({
+  declarations: [
+    ContactComponent,
+    ExperienciasComponent,
+    HabilidadesComponent,
+    InicioComponent,
+    MenuComponent,
+    PortifolioComponent,
+    SobreComponent,
+    PrincipalComponent,
+    ItemComponent,
+    SkillComponent,
+    ItemPortifolioComponent,
+    ScrollAnimationDirective
+  ],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    PrincipalComponent
+  ]
+})
+export class PrincipalModule { }
